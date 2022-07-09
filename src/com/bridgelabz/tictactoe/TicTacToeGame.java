651f5d9c273880373d12ebf025f44a7371bd81ae
Atulmahajan12345 <vishal.business0504@gmail.com>
2022-07-09 09:24:37 +0530

    Added: Asked Player letter and set computer letter

--- src/com/bridgelabz/tictactoe/TicTacToeGame.java
+++ src/com/bridgelabz/tictactoe/TicTacToeGame.java
@@ -4,42 +4,53 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 
-	public static void main(String[] args)
-	{
-		Scanner scanner = new Scanner(System.in);		
-		char[] board = createBoard();
-		System.out.println("Enter The Sysmbol which you want i.e. X or O");
-		char playerSymbol = scanner.next().charAt(0);
-		scanner.close();
-		char computerSymbol = determineComputerSymbol(playerSymbol);
+	/*public static void main(String[] args) {
+		createBoard();
 	}
 
-	private static char determineComputerSymbol(char playerSymbol) {
-		char computerSymbol = ' ' ;
-		if (playerSymbol == 'X')
-		{
-			computerSymbol = 'O';
-		}
-		else if (playerSymbol == 'O')
-		{
-			computerSymbol = 'X';
-		}
-		else
-		{
-			System.err.println("You inserted a wrong Symbol");
-		}
-		return computerSymbol;
-	}
-
-	private static char[] createBoard()
-	{
+	private static void createBoard() {
 		char[] board = new char[10];
-		for (int i = 1; i < board.length; i++) 
-		{
+		for (int i = 1; i < board.length; i++) {
 			board[i] = '-';
 		}
-		return board;
+	}*/
+
+public static void main(String[] args)
+{
+	Scanner scanner = new Scanner(System.in);		
+	char[] board = createBoard();
+	System.out.println("Enter The Sysmbol which you want i.e. X or O");
+	char playerSymbol = scanner.next().charAt(0);
+	scanner.close();
+	char computerSymbol = determineComputerSymbol(playerSymbol);
+}
+
+private static char determineComputerSymbol(char playerSymbol) {
+	char computerSymbol = ' ' ;
+	if (playerSymbol == 'X')
+	{
+		computerSymbol = 'O';
 	}
+	else if (playerSymbol == 'O')
+	{
+		computerSymbol = 'X';
+	}
+	else
+	{
+		System.err.println("You inserted a wrong Symbol");
+	}
+	return computerSymbol;
+}
+
+private static char[] createBoard()
+{
+	char[] board = new char[10];
+	for (int i = 1; i < board.length; i++) 
+	{
+		board[i] = '-';
+	}
+	return board;
+}
 
 
 }
